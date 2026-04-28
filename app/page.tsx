e06f5b4f85df780c69ec0921e893ab777f33d78f
Gerard Veenhof <gerard.veenhof@wur.nl>
2026-04-28 14:03:10 +0200

    force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import BlobList from '@/app/_components/blob-list';
 import UploadForm from '@/app/_components/upload-form';
 
+export const dynamic = 'force-dynamic'
+
 export default function Home() {
   return (
     <main className="mx-auto w-full max-w-lg flex flex-col gap-6 px-6 py-12">
